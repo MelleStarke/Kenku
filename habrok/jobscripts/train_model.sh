bc05dcc1fc332982ed27850df1080dd7c9b00767
MelleStarke <mellestarke@gmail.com>
2025-03-18 16:26:59 +0100

    Removed requirements from habrok/requirements.txt that can be loaded as modules instead

--- habrok/jobscripts/train_model.sh
+++ habrok/jobscripts/train_model.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --partition gpu
-#SBATCH --cpus-per-task 4
-#SBATCH --mem 128G
-#SBATCH --time 0-5:0:00
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 10G
+#SBATCH --time 0-3:30:00
 #SBATCH --nodes 1
 #SBATCH --gpus-per-node=a100:1
 
@@ -11,20 +11,12 @@
 #SBATCH --output="/home3/s4984218/Kenku/habrok/jobscripts/results/train_kenkuteacher_%j.out"
 
 module purge
-module load PyTorch-bundle/2.1.2-foss-2023a-CUDA-12.1.1
-module load scikit-learn/1.3.1-gfbf-2023a
 
-echo "Finished loading modules"
-
-source ~/venvs/py3.11.5/bin/activate
-
-echo "Activated venv"
+source ~/scripts/activate_venv.sh
 
 echo "Running script"
 
 cd ~/Kenku
-srun python -m train.train_model --dataset-dir ~/scratch/processed --preload-melspecs --n-cores $SLURM_CPUS_PER_TASK --min-samples 7 --conv-ch 512 --att-ch 512 -dor 0.2 --epochs 20 --batch-size 50 --max-test-batches 150 --run-dir ~/scratch/runs/$SLURM_JOB_ID
+srun python -m train.train_model --dataset-dir ~/scratch/processed --preload-melspecs --n-cores $SLURM_CPUS_PER_TASK --min-samples 7 --stack-factor 1 --conv-ch 512 --att-ch 512 -dor 0.2 --epochs 20 --batch-size 64 --max-test-batches 150 --run-dir ~/scratch/runs/$SLURM_JOB_ID
 
 echo "Finished training"
-
-deactivate
