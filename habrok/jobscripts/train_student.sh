9190b7985dfb331013208093988c93c0a98609ab
MelleStarke <mellestarke@gmail.com>
2025-04-22 17:18:17 +0200

    Split train_model.sh into train_teacher and train_student

--- habrok/jobscripts/train_student.sh
+++ habrok/jobscripts/train_student.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --partition gpu
 #SBATCH --cpus-per-task 1
-#SBATCH --mem 10G
-#SBATCH --time 0-1:00:00
+#SBATCH --mem 8G
+#SBATCH --time 0-5:00:00
 #SBATCH --nodes 1
 #SBATCH --gpus-per-node=1
 
-#SBATCH --job-name="train_kenkuteacher"
-#SBATCH --output="/home3/s4984218/Kenku/habrok/jobscripts/results/train_kenkuteacher_%j.out"
+#SBATCH --job-name="train_kenkustudent"
+#SBATCH --output="/home3/s4984218/Kenku/habrok/jobscripts/results/train_kenkustudent_%j.out"
 
 module purge
 
@@ -18,7 +18,7 @@ echo "Running script"
 
 cd ~/Kenku
 # srun python -m train.train_model --dataset-dir ~/scratch/processed --preload-melspecs --n-cores $SLURM_CPUS_PER_TASK --min-samples 7 --stack-factor 1 --conv-ch 512 --att-ch 512 -dor 0.2 --epochs 20 --batch-size 64 --max-test-batches 150 --run-dir ~/scratch/runs/$SLURM_JOB_ID
-srun python -m train.train_model --dataset-dir ~/scratch/processed --n-cores $SLURM_CPUS_PER_TASK --min-samples 7 --stack-factor 4 --conv-ch 80 --att-ch 80 -dor 0.2 --epochs 20 --batch-size 256 --max-test-batches 100 --run-dir ~/scratch/runs/$SLURM_JOB_ID
+srun python -m train.train_model --model-class student --from-teacher "~/scratch/runs/15714243/checkpoints/epoch49_batch0_loss0.5417.pt" --dataset-dir ~/scratch/processed --n-cores $SLURM_CPUS_PER_TASK --min-samples 7 --stack-factor 4 --conv-ch 512 --att-ch 512 -dor 0.2 --epochs 20 --batch-size 128 --max-test-batches 100 --run-dir ~/scratch/runs/student/$SLURM_JOB_ID
 
 echo "Finished training"
 
