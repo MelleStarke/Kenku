fd42c85040496ca7d2f2bc1fcf551be2b6511754
MelleStarke <mellestarke@gmail.com>
2025-04-23 14:12:51 +0200

    Student training with NaN loss

--- habrok/jobscripts/train_student.sh
+++ habrok/jobscripts/train_student.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --partition gpu
-#SBATCH --cpus-per-task 1
+#SBATCH --cpus-per-task 2
 #SBATCH --mem 8G
-#SBATCH --time 0-5:00:00
+#SBATCH --time 0-1:30:00
 #SBATCH --nodes 1
 #SBATCH --gpus-per-node=1
 
@@ -18,10 +18,10 @@ echo "Running script"
 
 cd ~/Kenku
 # srun python -m train.train_model --dataset-dir ~/scratch/processed --preload-melspecs --n-cores $SLURM_CPUS_PER_TASK --min-samples 7 --stack-factor 1 --conv-ch 512 --att-ch 512 -dor 0.2 --epochs 20 --batch-size 64 --max-test-batches 150 --run-dir ~/scratch/runs/$SLURM_JOB_ID
-srun python -m train.train_model --model-class student --from-teacher "~/scratch/runs/15714243/checkpoints/epoch49_batch0_loss0.5417.pt" --dataset-dir ~/scratch/processed --n-cores $SLURM_CPUS_PER_TASK --min-samples 7 --stack-factor 4 --conv-ch 512 --att-ch 512 -dor 0.2 --epochs 20 --batch-size 128 --max-test-batches 100 --run-dir ~/scratch/runs/student/$SLURM_JOB_ID
+srun python -m train.train_model --model-class student --from-teacher ~/scratch/runs/15714243/checkpoints/epoch49_batch0_loss0.5417.pt --dataset-dir ~/scratch/processed --n-cores $SLURM_CPUS_PER_TASK --min-samples 7 --stack-factor 4 --conv-ch 512 --att-ch 512 -dor 0.2 --epochs 20 --batch-size 128 --max-test-batches 100 --run-dir ~/scratch/runs/student/$SLURM_JOB_ID
 
 echo "Finished training"
 
 
-
+# python -m train.train_model --model-class student --from-teacher ~/scratch/runs/15714243/checkpoints/epoch49_batch0_loss0.5417.pt --dataset-dir ~/scratch/processed --n-cores 2 --min-samples 7 --stack-factor 4 --conv-ch 512 --att-ch 512 -dor 0.2 --epochs 20 --batch-size 128 --max-test-batches 100 --run-dir ~/scratch/runs/student/int_gpu1
 # --dataset-dir ../Data/processed/VCTK --n-cores 6 --min-samples 7 --stack-factor 4 --conv-ch 80 --att-ch 80 -dor 0.2 --epochs 20 --batch-size 18 --max-test-batches 60
