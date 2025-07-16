5ac3e5d8adde01da2eff6644416bacddd5ff1a37
Melle Starke <mellestarke@tuta.io>
2025-07-16 15:36:24 +0200

    - Record hypertune observations - Allow host specification for view_tb_stream.sh

--- scripts/view_tb_stream.sh
+++ scripts/view_tb_stream.sh
@@ -2,12 +2,27 @@
 
 # Default port value
 PORT=47761
+HOST="habrokint2"
 
-# Check if a port is provided as second argument
-if [ "$2" = "--port" ] && [ ! -z "$3" ]; then
-  PORT=$3
-fi
+# Check if a port is provided
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -p|--port)
+      PORT="$2"
+      shift 2
+      ;;
+    -h|--host)
+      HOST="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option: $1"
+      echo "Usage: $0 [-h|--host hostname] [-d|--dir logdir]"
+      exit 1
+      ;;
+  esac
+done
 
 # Create the SSH tunnel
 echo "[INFO] Creating SSH tunnel from localhost:6006 to $1:localhost:$PORT"
-ssh -L 6006:localhost:$PORT "$1"
+ssh -L 6006:localhost:$PORT $HOST
