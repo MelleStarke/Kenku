0c7d447fabe8d7ab8a3a51dd63dc99731ebdca48
MelleStarke <mellestarke@gmail.com>
2025-10-13 17:57:17 +0200

    Created pre-made settings dirs for easy repetition. Made the hypertune script able to work with json files instead of src code editing

--- train/hypertune.py
+++ train/hypertune.py
@@ -4,7 +4,7 @@ import subprocess
 
 from itertools import product
 
-from data.util import save_config
+from data.util import save_config, load_config
 
 variable_settings = {
   'stack_factor': [2, 4, 8],
@@ -43,7 +43,9 @@ def get_setting(config_dict, idx):
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Hyperparameter tuning for the model.')
-  parser.add_argument('job_dir', type=str, metavar='DIR',
+  parser.add_argument('setting_dir', type=str, metavar='STR',
+                      help="Directory containing `static_settings.json` and `variable_settings.json', from which to construct the configs for each run.")
+  parser.add_argument('job_dir', type=str, metavar='STR',
                       help="Directory of the entire hypertuning task")
   parser.add_argument('setting_index', type=int, metavar='INT',
                       help="Index of the hyperparameter setting to run")
@@ -52,11 +54,23 @@ if __name__ == "__main__":
 
   args = parser.parse_args()
 
+  static_settings = load_config(os.path.join(args.setting_dir, 'static_settings.json'))
+  variable_settings = load_config(os.path.join(args.setting_dir, 'variable_settings.json'))
+  
+  # Make sure there is no overlap between the static and variable settings
+  static_keys_set = set(static_settings.keys())
+  variable_keys_set = set(variable_settings.keys())
+  keys_union = static_keys_set & variable_keys_set
+  if len(keys_union) > 0:
+    raise ValueError('There is at least one setting set as both static and variable. Make sure each setting is either static or variable' \
+                    f'Setting(s) in question: {keys_union}')
   
   if args.setting_index == 1:
-    save_config(variable_settings, os.path.join(args.job_dir, 'hypertune_settings.json'))
-  
+    save_config(static_settings, os.path.join(args.job_dir, 'static_settings.json'))
+    save_config(variable_settings, os.path.join(args.job_dir, 'variable_settings.json'))
+    
   this_setting = get_setting(variable_settings, args.setting_index)
+  
   print("Setting: ", this_setting)
   
   # Make descriptive name of this run's folder
@@ -65,36 +79,81 @@ if __name__ == "__main__":
   # Make directory and save configs
   os.makedirs(run_dir, exist_ok=True)
 
-  dataset_config = {
-    'dataset_dir': '/home3/s4984218/scratch/processed',
-    'n_cores': args.n_cores,
-    'min_samples': 5,
-    'train_set_threshold': 10,
-  }
+  this_setting.update(static_settings)
   
+  #=== Process Hidden Channels Individually or In Tandem ===#
   sf = this_setting['stack_factor']
-  model_config = {
-    'conv_ch': this_setting['hidden_ch'] * sf,
-    'att_ch': this_setting['hidden_ch'] * sf,
-    'embed_ch': 16,
-    'stack_factor': sf,
-    'dropout_rate': this_setting['dropout_rate'],
-  }
+  if all(channel in this_setting for channel in ['conv_ch', 'att_ch'])
+    for channel in ['conv_ch', 'att_ch']:
+      this_setting[channel] = this_setting[channel] * sf
+      
+  elif 'hidden_ch' in this_setting:
+    for channel in ['conv_ch', 'att_ch']:
+      this_setting[channel] = this_setting['hidden_ch'] * sf
+      
+  else:
+    raise ValueError('You need to specify the convolutional and attention channels either individually ' \
+                     'as `conv_ch` and `att_ch`, or in tandem as `hidden_ch`.')
+  
+  #=== Process Attention Weights ===#
+  if 'att_weight' in this_setting:
+    for weight in ['DAL_weight', 'OAL_weight']:
+      this_setting[weight] = this_setting['att_weight']
+      
+  else:
+    raise ValueError('You need to specify the diagonal and orthogonal attention loss weights in tandem with `att_weight`.')
+  
+  #=== Settings Specified at Runtime ===#
+  if 'n_cores' in this_setting:
+    raise ValueError('`n_cores` should be specified through the shell script directly, not in the .json files.')
+  this_setting['n_cores'] = args.n_cores,
+  
+  if 'run_dir' in this_setting:
+    raise ValueError('`run_dir` is decided at runtime dynamically by the hypertune script.')
+  this_setting['run_dir'] = run_dir
+  
+  #=== Construct Config Dicts ===#
+  dataset_config = {k: this_setting[k] for k [
+    'dataset_dir',
+    'n_cores',
+    'min_samples',
+    'train_set_threshold',
+    'sample_pairing',
+    'no_downsample',
+    'preload_melspecs'
+  ]}
+  
+  model_config = {k: this_setting[k] for k [
+    'model_class',
+    'drl',
+    'from_teacher',
+    'in_ch',
+    'conv_ch',
+    'att_ch',
+    'out_ch',
+    'embed_ch',
+    'num_accents',
+    'stack_factor',
+    'dropout_rate'
+  ]}
 
-  train_config = {
-    'epochs': 60,
-    'batch_size': 500,
-    'main_loss': 'mse',
-    'learning_rate': this_setting['learning_rate'],
-    'DAL_weight': this_setting['att_weight'],
-    'OAL_weight': this_setting['att_weight'] if this_setting['OAL_weight_on'] else 0,
-    'att_weight_decay': this_setting['att_weight_decay'],
-    'test_interval': 85,
-    'melspec_interval': 85,
-    'checkpoint_interval': 170,
-    'checkpoint_max': 2,
-    'run_dir': run_dir,
-  }
+  train_config = {k: this_setting[k] for k [
+    'epochs',
+    'batch_size',
+    'main_loss',
+    'learning_rate',
+    'adam_betas',
+    'DAL_weight',
+    'OAL_weight',
+    'att_weight_decay',
+    'test_interval',
+    'melspec_interval',
+    'max_test_batches',
+    'run_dir',
+    'checkpoint_interval',
+    'checkpoint_max',
+    'from_checkpoint'
+  ]}
   
   # Save configs to the run directory
   for name, config in zip(['dataset', 'model', 'train'], [dataset_config, model_config, train_config]):
