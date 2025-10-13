a504c72a3f7eee17a40036f442ec5055d7ecd1cb
MelleStarke <mellestarke@gmail.com>
2025-10-13 23:18:35 +0200

    Fixed jobscript write location for drl teacher

--- habrok/jobscripts/train_drl_teacher.sh
+++ habrok/jobscripts/train_drl_teacher.sh
@@ -17,7 +17,7 @@ source ~/scripts/activate_venv.sh
 
 echo "Script Contents"
 
-cat ~/Kenku/habrok/jobscripts/train_teacher.sh
+cat ~/Kenku/habrok/jobscripts/train_drl_teacher.sh
 
 echo "Running script"
 date +"%H:%M:%S"
@@ -26,7 +26,7 @@ date +"%H:%M:%S"
 
 cd ~/Kenku
 # srun --export=ALL python -m train.train_model -lr 1e-6 -wda 2000 -woa 2000 --test-interval 50 --melspec-interval 50 --checkpoint-interval 100 --checkpoint-max 5 --dataset-dir ~/scratch/processed --n-cores $SLURM_CPUS_PER_TASK --min-samples 7 --stack-factor 1 --conv-ch 512 --att-ch 256 -dor 0.2 --epochs 200 --main-loss mse --batch-size 200 --max-test-batches 200 --run-dir ~/scratch/runs/teacher/tuned/$SLURM_JOB_ID
-srun --export=ALL python -m train.train_model --config-dir /home3/s4984218/Kenku/train/configs/best_teacher --run-dir ~/scratch/runs/drl_teacher/sanity_check_$SLURM_JOB_ID --batch-size 600 --epochs 200 --checkpoint-max 5 --drl
+srun --export=ALL python -m train.train_model --config-dir ./train/configs/best_teacher --run-dir ~/scratch/runs/drl_teacher/sanity_check_$SLURM_JOB_ID --batch-size 600 --epochs 200 --checkpoint-max 5 --drl
 
 echo "Finished training"
 
