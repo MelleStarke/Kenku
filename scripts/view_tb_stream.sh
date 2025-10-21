c03309590bc52178d9c6a06cdea0d8c07c276bfd
Melle Starke <mellestarke@tuta.io>
2025-10-21 16:30:52 +0200

    - Added local port specification for view_tb_stream.sh

--- scripts/view_tb_stream.sh
+++ scripts/view_tb_stream.sh
@@ -3,6 +3,7 @@
 # Default port value
 PORT=47761
 HOST="habrokint2"
+LOCAL_PORT=6006
 
 # Check if a port is provided
 while [[ $# -gt 0 ]]; do
@@ -15,9 +16,13 @@ while [[ $# -gt 0 ]]; do
       HOST="$2"
       shift 2
       ;;
+    -l|--localport)
+      LOCAL_PORT="$2"
+      shift 2
+      ;;
     *)
       echo "Unknown option: $1"
-      echo "Usage: $0 [-h|--host hostname] [-d|--dir logdir]"
+      echo "Usage: $0 [-h|--host hostname] [-d|--dir logdir] [-l|--localport local_port]"
       exit 1
       ;;
   esac
@@ -25,4 +30,4 @@ done
 
 # Create the SSH tunnel
 echo "[INFO] Creating SSH tunnel from localhost:6006 to $1:localhost:$PORT"
-ssh -L 6006:localhost:$PORT $HOST
+ssh -L $LOCAL_PORT:localhost:$PORT $HOST
